a6550c8da7a60be149a1e89b9febbc56fd50e7dd
schesterx64 <zedabriyan17@gmail.com>
2026-09-21 13:18:24 +0700

    Initial Commit

---
+++ teori/01-introduction/helloword.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Selamat Datang di Kelas Algoritma Pemrograman")
+}
---
+++ teori/01-introduction/tipe_data.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var umur int8
+	var anakKe int8
+	var saldo int64
+
+	umur = 20
+	anakKe = 1
+	saldo = 1000000000
+
+	fmt.Println("Umur saya adalah", umur, "tahun")
+	fmt.Println("Saya anak ke", anakKe)
+	fmt.Println("Saldo saya adalah", saldo, "rupiah")
+}
